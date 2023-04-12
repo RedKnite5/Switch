0ff52248aec443b883259b34e0e018e5849325a3
Max Friedman <mr.awesome10000@gmail.com>
2023-04-11 20:42:00 -0700

    old changes to master

---
+++ Switch/out.py
@@ -0,0 +1,3 @@
+from Switch.switch_builtins import *
+main_ns = Namespace()
+print_no_nl(SwitchFrac(1))
---
+++ Switch/tests/out.py
@@ -0,0 +1,6 @@
+from Switch.switch_builtins import *
+namespace = Namespace()
+while SwitchFrac(0):
+ print_no_nl(SwitchFrac(1))
+
+
